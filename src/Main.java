8bee1fd545f0013c137f1e7929411e857cae3832
Giorgi Chapidze <chapidzeee.giorgi@gmail.com>
2025-04-03 10:11:53 +0400

    Implement: `merge`

--- src/Main.java
+++ src/Main.java
@@ -16,4 +16,33 @@ public class Main {
             throw new RuntimeException(e);
         }
     }
+
+    private static void merge(final List<Path> tempFiles) throws IOException {
+        var words = new TreeSet<>(String::compareToIgnoreCase);
+        var buffer = CharBuffer.allocate((int) Files.size(tempFiles.getFirst()));
+
+        for (Path tempFile : tempFiles) {
+            File file = tempFile.toFile();
+
+            try (var reader = new BufferedReader(new InputStreamReader(new FileInputStream(file)))) {
+                reader.read(buffer);
+
+                var filteredWords =
+                        Arrays.stream(new String(buffer.array()).split(" "))
+                                .filter(Predicate.not(String::isBlank))
+                                .map(String::trim)
+                                .toList();
+
+                words.addAll(filteredWords);
+
+                buffer.flip();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        try (var writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("resources/sonnets_sorted.txt")))) {
+            writer.write(String.join(" ", words));
+        }
+    }
 }
